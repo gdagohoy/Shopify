98cf4d2ea3ff4ec00489265070cbf9c0c5c4fe7e
glaiza <glaiza.dagohoy@carsu.edu.ph>
2025-05-06 11:21:32 +0800

    added dashboard

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../views/LoginView.vue'
 import RegisterView from '../views/RegisterView.vue'
+import DashboardView from '../views/system/DashboardView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -13,6 +14,12 @@ const router = createRouter({
       path: '/register',
       name: 'register',
       component: RegisterView,
+    },
+
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: DashboardView,
     },
   ],
 })
---
+++ src/views/system/DashboardView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import AppLayout from '@/components/layout/AppLayout.vue'
+</script>
+
+<template>
+  <AppLayout>
+    <template #content>
+      <h1 class="text-center">Rent Well</h1>
+    </template>
+  </AppLayout>
+</template>
